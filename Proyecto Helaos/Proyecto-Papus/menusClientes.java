93844637dbdafd6713039bae5e2e637ba177502e
Andrew Martínez <4ndrewmartinez02@gmail.com>
2024-11-28 20:45:52 +0100

    Add files via upload
    V1.0 del Menu de clientes

---
+++ Proyecto Helaos/Proyecto-Papus/menusClientes.java
@@ -0,0 +1,13 @@
+public class menusClientes {
+
+    public void mostrarMenu()
+    {
+        System.out.println("1. Helados fríos");
+        System.out.println("---------------");
+        System.out.println("2. Granizao");
+        System.out.println("---------------");
+        System.out.println("3. Gofres");
+        System.out.println("---------------");
+        System.out.println("4. Bebias");
+    }
+}
