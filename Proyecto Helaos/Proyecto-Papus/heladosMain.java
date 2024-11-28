622adab89bec6dfeb17dcf973594ae4f83cc7b6b
Andrew Martínez <4ndrewmartinez02@gmail.com>
2024-11-28 20:45:52 +0100

    Add files via upload
    V1.0 del Menu de clientes

---
+++ Proyecto Helaos/Proyecto-Papus/heladosMain.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class heladosMain {
+    public static void main(String[] args) {
+        
+    }
+}
--- Proyecto Helaos/Proyecto-Papus/menusClientes.java
+++ Proyecto Helaos/Proyecto-Papus/menusClientes.java
@@ -1,13 +1,25 @@
 public class menusClientes {
 
     public void mostrarMenu()
-    {
-        System.out.println("1. Helados fríos");
-        System.out.println("---------------");
-        System.out.println("2. Granizao");
-        System.out.println("---------------");
-        System.out.println("3. Gofres");
-        System.out.println("---------------");
-        System.out.println("4. Bebias");
-    }
+
+    {int numPedido = 5;
+        
+        do { System.out.println("1. Helados fríos");
+ 
+         System.out.println("---------------");
+         System.out.println("2. Granizao");
+ 
+         System.out.println("---------------");
+         System.out.println("3. Gofres");
+ 
+         System.out.println("---------------");
+         System.out.println("4. Bebias");
+ 
+         System.out.println("---------------");
+         System.out.println("Escribe una opción 1-4 o pulse 0 para salir del menú");
+         
+         
+        } while (numPedido != 0);
+        
+     }
 }
