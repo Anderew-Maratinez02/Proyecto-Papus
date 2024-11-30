a9fe51927e0ac829b829852bca454bca05b38837
Andrew Martínez <4ndrewmartinez02@gmail.com>
2024-11-30 17:15:18 +0000

    Proyecto-papus

--- Proyecto Helaos/Proyecto-Papus/menusClientes.java
+++ Proyecto Helaos/Proyecto-Papus/menusClientes.java
@@ -3,7 +3,7 @@ public class menusClientes {
     public void mostrarMenu()
 
     {int numPedido = 5;
-        
+
         do { System.out.println("1. Helados fríos");
  
          System.out.println("---------------");
@@ -18,6 +18,7 @@ public class menusClientes {
          System.out.println("---------------");
          System.out.println("Escribe una opción 1-4 o pulse 0 para salir del menú");
          
+          
          
         } while (numPedido != 0);
         
